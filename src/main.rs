58e888a73b43e77f2185c5725567f958c73bd64f
MrNamra <namraramsha@yahoo.com>
2026-04-23 22:46:33 +0530

    learing how to genareate randome funtion infinit loop cargo packages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
-fn main() {
-    test();
-}
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-fn test () {
-    println!("can I do it?");
+fn main() {
+    println!("Guess The number!");
+
+    let secret_no = rand::thread_rng().gen_range(1..=100);
+    println!("the secreat No is {secret_no}");
+
+    loop {
+
+        println!("Guess a number: ");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fail to read user input!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please Enter Valid Input");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_no) {
+            Ordering::Equal => {
+                println!("You Won!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too Small!"),
+        }
+    }
 }

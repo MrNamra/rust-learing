34420b51bb26f373ecd6015325cbfe91847c0d1f
MrNamra <namraramsha@yahoo.com>
2026-04-25 20:20:58 +0530

    learing about shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 fn main() {
-    let mut age = 22;
+    let age: i32 = 22;
     println!("My Age is {age}");
 
-    age = 15;
-    println!("My mutables Age is  {age}");
-
-    const ONE_HOUR: u32 = 60 * 60;
-
-    const THREE_HOURS: u32 = ONE_HOUR * 3;
-
-    println!("Three Hours In seconds is {THREE_HOURS}")
-    
+    let age: bool = true;
+    println!("Showing age variable {age}")
 }

b3d0ee9978b84441d0e4bb7428402b597faf232b
MrNamra <namraramsha@yahoo.com>
2026-04-27 22:35:49 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 fn main() {
-    let x = add(5, 4);
-
-    println!("x value is {x}");
-
-    let new = min(10, 4);
-    println!("new value is {new}");
-
-    let var_check = is_even(5);
+    let var_check = is_even(4);
     println!("does value is even? {var_check}");
-}
 
-fn add(x: u32, y: u32) -> u32 {
-    return x + y;
-}
-
-fn min(x: u32, y: u32) -> u32 {
-    x - y
+    if var_check {
+        println!("fn return true")
+    }
 }
 
 fn is_even(x: u32) -> bool {

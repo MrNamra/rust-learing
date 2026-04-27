612de7707f616e455ca5360752575ce90e7b2374
MrNamra <namraramsha@yahoo.com>
2026-04-27 22:50:59 +0530

    leand about loops

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
 fn main() {
-    let var_check = is_even(4);
-    println!("does value is even? {var_check}");
+    let mut num = 1;
+    loop {
+        println!("Value of number is {}", num);
+        
+        if num == 10 {
+            break;
+        }
 
-    if var_check {
-        println!("fn return true")
+        num += 1;
     }
+
+    println!("Loop Ended");
+
+    loop1();
+
+    loop2();
 }
 
-fn is_even(x: u32) -> bool {
-    if x % 2 == 0 {
-        return true;
+fn loop1() {
+    let mut n = 3;
+
+    while n != 0 {
+        println!("{n}");
+
+        n -=1;
     }
-    false
+
+    println!("end while loop");
+
+    let arr = [1,2,3,4,5,6];
+
+    let mut index = 0;
+
+    while index < 6 {
+        println!("i: {} and v:{}", index, arr[index]);
+        index +=1;
+    }
+
+}
+
+fn loop2() {
+    let arr = [1,2,3,4,5];
+
+    for x in arr {
+        println!("x = {}", x)
+    }
 }

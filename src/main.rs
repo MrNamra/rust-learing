9d3d46f19c9ca3eaf053a2acb7ff65d6578217ae
MrNamra <namraramsha@yahoo.com>
2026-04-27 22:06:45 +0530

    function learing in rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 fn main() {
-    let space = "    ";
-    let space = space.len();
-    println!("length is {space}");
-    my_function();
+    let x = add(5, 4);
+
+    println!("x value is {x}");
+
+    let new = min(10, 4);
+    println!("new value is {new}");
+
+    let var_check = is_even(5);
+    println!("does value is even? {var_check}");
 }
 
-fn my_function() {
-    println!("Hello From my Function");
+fn add(x: u32, y: u32) -> u32 {
+    return x + y;
+}
+
+fn min(x: u32, y: u32) -> u32 {
+    x - y
+}
+
+fn is_even(x: u32) -> bool {
+    if x % 2 == 0 {
+        return true;
+    }
+    false
 }
